d5b5794dc4adad77f943faf5ddcd1b854571c6ea
Giacomo <giacomo@dhcp24-ans.wifi.uit.no>
2012-07-27 14:20:57 +0200

    Add in place

--- vector4.go
+++ vector4.go
@@ -26,6 +26,15 @@ func (v1 Vector4) Add(v2 Vector4) Vector4 {
 	return Vector4{v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2], v1[3] + v2[3]}
 }
 
+// Add in place v1 += v2
+func (v1 Vector4) IAdd(v2 Vector4) Vector4 {
+    v1[0]+=v2[0]
+    v1[1]+=v2[1]
+    v1[2]+=v2[2]
+    v1[3]+=v2[3]
+	return v1
+}
+
 // return v1-v2 (won't modify any of them)
 func (v1 Vector4) Sub(v2 Vector4) Vector4 {
 	return Vector4{v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2], v1[3] - v2[3]}
